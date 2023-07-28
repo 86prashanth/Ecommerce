00f21c935f7a1588f979be6d416daac856f291f5
Ambala Prashanth <89448613+86prashanth@users.noreply.github.com>
2023-07-28 16:40:21 +0530

    Update checkout.js

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
                 success: function (response) {
                     var options = {
                         "key": "rzp_test_peAOxXTKroZdOB", // Enter the Key ID generated from the Dashboard
-                        "amount": 1*100
+                        "amount": 1*100,
                         // "amount": response.total_price * 100, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
                         "currency": "INR",
                         "name": "Developer", //your business name
